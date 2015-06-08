7c018f564ec6059d22cfb8761679d4eae8ee705b
DanielRanLehmann <danielran11@gmail.com>
2015-06-08 14:49:14 +0200

    Initial Commit

---
+++ ColorFromHex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorFromHex
+//
+//  Created by Daniel Ran Lehmann on 08/06/15.
+//  Copyright (c) 2015 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
